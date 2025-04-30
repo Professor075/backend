b59a993ab1785efd372a59597eda582c7d6c01ae
Professor075 <117261215+Professor075@users.noreply.github.com>
2025-04-30 18:42:14 +0500

    Update server.js

--- server.js
+++ server.js
@@ -57,6 +57,6 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
